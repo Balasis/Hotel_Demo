d336bfb6045b2478b3dbbb2f08b84bd671b3d19d
Balasis_Ioannis <giovani1994a@gmail.com>
2025-02-27 00:35:53 +0200

    moved down the findall from base to the extended classes,Req mapping of reservationController is now under guest/{guestId}/ , created findByGuestId.

--- hotel-core/src/main/java/gr/balasis/hotel/core/controller/ReservationController.java
+++ hotel-core/src/main/java/gr/balasis/hotel/core/controller/ReservationController.java
@@ -11,6 +11,7 @@ import gr.balasis.hotel.core.service.ReservationService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -19,14 +20,15 @@ import java.util.stream.Collectors;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/reservations")
+@RequestMapping("guests/{guestId}/reservations")
 public class ReservationController extends BaseController<Reservation, ReservationResource, ReservationEntity> {
     private final ReservationService reservationService;
     private final ReservationMapper reservationMapper;
 
     @GetMapping
-    public ResponseEntity<List<ReservationResource>> findAll() {
-        List<ReservationResource> resources = reservationService.findAll()
+    public ResponseEntity<List<ReservationResource>> findByGuestId(
+            @PathVariable final Long guestId) {
+        List<ReservationResource> resources = reservationService.findByGuestId(guestId)
                 .stream()
                 .map(getMapper()::toResource)
                 .collect(Collectors.toList());
--- hotel-core/src/main/java/gr/balasis/hotel/core/tests/Mappers.java
+++ hotel-core/src/main/java/gr/balasis/hotel/core/tests/Mappers.java
@@ -9,6 +9,7 @@ import gr.balasis.hotel.core.entity.RoomEntity;
 import gr.balasis.hotel.core.mapper.GuestMapper;
 import gr.balasis.hotel.core.mapper.ReservationMapper;
 import gr.balasis.hotel.core.mapper.RoomMapper;
+import gr.balasis.hotel.core.service.GuestService;
 import lombok.AllArgsConstructor;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Bean;
@@ -21,11 +22,12 @@ public class Mappers {
     private final GuestMapper guestMapper;
     private final RoomMapper roomMapper;
     private final ReservationMapper reservationMapper;
+    private final GuestService guestService;
 
     @Bean
     public CommandLineRunner run() {
         return args -> {
-            //mappers are disabled from using the builders, it wouldn't pass parent fields(skips Id)
+            //mappers are disabled from using the builders, it wouldn't pass parent fields(builders' use skips Id)
             GuestEntity guestEntity = GuestEntity.builder()
                     .firstName("john")
                     .lastName("balasis")
@@ -34,31 +36,32 @@ public class Mappers {
             guestEntity.setId(325L);
             Guest guest = guestMapper.toDomainFromEntity(guestEntity);
 
-            RoomEntity roomEntity = RoomEntity.builder()
-                    .pricePerNight(new BigDecimal("100"))
-                    .roomNumber("r12")
-                    .build();
-            roomEntity.setId(125L);
-            Room room = roomMapper.toDomainFromEntity(roomEntity);
-
-            ReservationEntity reservationEntity = ReservationEntity.builder()
-                    .roomId(125L)
-                    .guestId(325L)
-                    .build();
-            reservationEntity.setId(1L);
-            Reservation reservation = reservationMapper.toDomainFromEntity(reservationEntity);
-
-            System.out.println("Guest domain id is :  "  + guest.getId() );
-            System.out.println("Guest entity id is :  "  + guestMapper.toEntity(guest).getId() );
-            System.out.println("Guest resource id is: "  + guestMapper.toResource(guest).getId() );
-
-            System.out.println("Room domain id is :  "  + room.getId() );
-            System.out.println("Room entity id is :  "  + roomMapper.toEntity(room).getId() );
-            System.out.println("Room resource id is: "  + roomMapper.toResource(room).getId() );
-
-            System.out.println("Reservation domain id is :  "  + reservation.getId() );
-            System.out.println("Reservation entity id is :  "  + reservationMapper.toEntity(reservation).getId() );
-            System.out.println("Reservation resource id is: "  + reservationMapper.toResource(reservation).getId() );
+//
+//            RoomEntity roomEntity = RoomEntity.builder()
+//                    .pricePerNight(new BigDecimal("100"))
+//                    .roomNumber("r12")
+//                    .build();
+//            roomEntity.setId(125L);
+//            Room room = roomMapper.toDomainFromEntity(roomEntity);
+//
+//            ReservationEntity reservationEntity = ReservationEntity.builder()
+//                    .roomId(125L)
+//                    .guestId(325L)
+//                    .build();
+//            reservationEntity.setId(1L);
+//            Reservation reservation = reservationMapper.toDomainFromEntity(reservationEntity);
+//
+//            System.out.println("Guest domain id is :  "  + guest.getId() );
+//            System.out.println("Guest entity id is :  "  + guestMapper.toEntity(guest).getId() );
+//            System.out.println("Guest resource id is: "  + guestMapper.toResource(guest).getId() );
+//
+//            System.out.println("Room domain id is :  "  + room.getId() );
+//            System.out.println("Room entity id is :  "  + roomMapper.toEntity(room).getId() );
+//            System.out.println("Room resource id is: "  + roomMapper.toResource(room).getId() );
+//
+//            System.out.println("Reservation domain id is :  "  + reservation.getId() );
+//            System.out.println("Reservation entity id is :  "  + reservationMapper.toEntity(reservation).getId() );
+//            System.out.println("Reservation resource id is: "  + reservationMapper.toResource(reservation).getId() );
 
         };
     }
