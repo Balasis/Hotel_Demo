00751e1e01970ee73eb9d2e346676b09af79c0f7
Balasis_Ioannis <giovani1994a@gmail.com>
2025-03-01 19:33:11 +0200

    removing the module itself from component scan as spring considers it already and causing issues to recognize beans of hotel-data

--- hotel-core/src/main/java/gr/balasis/hotel/core/HotelCore.java
+++ hotel-core/src/main/java/gr/balasis/hotel/core/HotelCore.java
@@ -5,7 +5,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
-@ComponentScan(basePackages = {"gr.balasis.hotel.context","gr.balasis.hotel.core","gr.balasis.hotel.data"})
+@ComponentScan(basePackages = {
+        "gr.balasis.hotel.context",
+        "gr.balasis.hotel.data"
+})
 public class HotelCore {
     public static void main(String[] args){
         SpringApplication.run(HotelCore.class, args);
